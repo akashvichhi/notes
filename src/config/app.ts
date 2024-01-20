f40291a937fcdde26c44beec9f2bbad54de15620
Akash Vichhi <akashvichhi@gmail.com>
2024-01-20 20:18:37 +0530

    install dependencies

---
+++ src/config/app.ts
@@ -0,0 +1,7 @@
+const config = {
+    appName: import.meta.env.VITE_APP_NAME,
+    baseUrl: import.meta.env.VITE_BASE_URL,
+    apiUrl: import.meta.env.VITE_API_URL,
+};
+
+export default config;
---
+++ src/config/cookie.ts
@@ -0,0 +1,7 @@
+import Cookies from 'universal-cookie';
+
+const cookies: Cookies = new Cookies(null, {
+    expires: (new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)),
+});
+
+export default cookies;

7356c0fd5deb049e19aa0c8dd4ed9b70318ca2cc
Akash Vichhi <akashvichhi@gmail.com>
2024-01-23 21:53:37 +0530

    rmeove unnecessary code from home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,12 @@
 import { Button } from "flowbite-react";
 import { useNavigate } from "react-router-dom";
-import { useAppDispatch, useAppSelector, useAuth } from "../app/hooks";
+import { useAppDispatch, useAuth } from "../app/hooks";
 import { logout } from "../features/authSlice";
-import { RootState } from "../app/store";
-import { useEffect } from "react";
 
 const HomePage = () => {
   const auth = useAuth();
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
-  const { isSuccess } = useAppSelector((state: RootState) => state.auth);
 
   const signout = async () => {
     await dispatch(logout());
@@ -17,14 +14,6 @@ const HomePage = () => {
       navigate("/login");
     });
   };
-
-  useEffect(() => {
-    if (isSuccess) {
-      // auth.signout(() => {
-      //   navigate("/login");
-      // });
-    }
-  }, [isSuccess]); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <div>

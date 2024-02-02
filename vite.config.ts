7c8e81b8ece0171feb54beed303a17e507cbf7c9
Akash Vichhi <akashvichhi@gmail.com>
2024-02-02 23:12:52 +0530

    build file names change

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,20 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  optimizeDeps: {
+    include: ["@workspace/ckeditor5-custom-build"],
+  },
+  build: {
+    commonjsOptions: {
+      include: [/@workspace\/ckeditor5-custom-build/, /node_modules/],
+    },
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[hash].[ext]",
+        chunkFileNames: "assets/[hash].js",
+        entryFileNames: "assets/[hash].js",
+      },
+    },
+  },
   plugins: [react()],
 });

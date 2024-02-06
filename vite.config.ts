7771c11c71c4bde5340f28f9c495b562c266e795
Akash Vichhi <akashvichhi@gmail.com>
2024-02-06 20:32:20 +0530

    remove unnecessary code

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  optimizeDeps: {
-    include: ["@workspace/ckeditor5-custom-build"],
-  },
   build: {
-    commonjsOptions: {
-      include: [/@workspace\/ckeditor5-custom-build/, /node_modules/],
-    },
     rollupOptions: {
       output: {
         assetFileNames: "assets/[hash].[ext]",

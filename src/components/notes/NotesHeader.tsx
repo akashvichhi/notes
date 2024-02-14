c6c812dda376997276de1a66268cfaaf418d7f18
Akash Vichhi <akashvichhi@gmail.com>
2024-02-14 19:38:31 +0530

    create notes header component

---
+++ src/components/notes/NotesHeader.tsx
@@ -0,0 +1,109 @@
+import { Button, Tooltip } from "flowbite-react";
+import { FiMenu, FiPlus, FiRotateCw, FiSave, FiX } from "react-icons/fi";
+import { useAppSelector } from "../../hooks/redux";
+import { RootState } from "../../store/store";
+import Note from "../../types/Note";
+import Loader from "../common/Loader";
+
+interface NotesHeaderProps {
+  showNoteList: boolean;
+	toggleNoteList: (show: boolean) => void;
+	note?: Note;
+	createNote: () => void;
+	reloadNote: () => void;
+	saveNote: () => void;
+}
+
+const NotesHeader = ({
+	showNoteList,
+	toggleNoteList,
+	note,
+	createNote,
+	reloadNote,
+	saveNote,
+}: NotesHeaderProps) => {
+	const { status, action } = useAppSelector(
+    (state: RootState) => state.notes,
+  );
+
+  return (
+    <div className="notes-header">
+      <div className="flex items-center">
+        {!showNoteList && (
+          <Button
+            color="dark"
+            size={"xs"}
+            className="md:hidden btn-save-note"
+            onClick={() => toggleNoteList(!showNoteList)}
+          >
+            <FiMenu size={20} />
+          </Button>
+        )}
+        {showNoteList && (
+          <Button
+            color="dark"
+            size={"xs"}
+            className="md:hidden btn-save-note btn-notes-list-close"
+            onClick={() => toggleNoteList(false)}
+          >
+            <FiX size={20} />
+          </Button>
+        )}
+        {note?.name ? (
+          <span className="notes-header-item">
+            {note?.name ?? ""}
+            {note?.isSaved === false && <sup className="text-sm">*</sup>}
+          </span>
+        ) : null}
+        <Tooltip content="Create new note" className="z-[9999]">
+          <Button
+            color="warning"
+            size={"xs"}
+            className="btn-create-note"
+            onClick={createNote}
+          >
+            <FiPlus size={20} />
+          </Button>
+        </Tooltip>
+      </div>
+      <div className="flex gap-1">
+        <Tooltip content="Reload" className="z-[9999]">
+          <Button
+            size={"xs"}
+            color="warning"
+            className="btn-save-note"
+            onClick={reloadNote}
+            isProcessing={status === "pending" && action === "fetch"}
+            processingSpinner={<Loader size={"sm"} />}
+            disabled={
+              (status === "pending" && action === "fetch") ||
+              !note ||
+              note.isDeleted
+            }
+          >
+            <FiRotateCw size={20} />
+          </Button>
+        </Tooltip>
+        <Tooltip content="Save" className="z-[9999]">
+          <Button
+            size={"xs"}
+            color="success"
+            className="btn-save-note"
+            onClick={saveNote}
+            isProcessing={status === "pending" && action === "save"}
+            processingSpinner={<Loader size={"sm"} />}
+            disabled={
+              (status === "pending" && action === "save") ||
+              !note ||
+              note.isDeleted
+            }
+          >
+            <FiSave size={20} />
+          </Button>
+        </Tooltip>
+      </div>
+    </div>
+  );
+};
+
+export default NotesHeader;
--- src/pages/Notes.tsx
+++ src/pages/Notes.tsx
@@ -1,13 +1,11 @@
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
-import { Button, Tooltip } from "flowbite-react";
 import { useCallback, useEffect, useMemo, useState } from "react";
-import { FiMenu, FiPlus, FiRotateCw, FiSave, FiX } from "react-icons/fi";
 import "../assets/css//components/_ckeditor.scss";
 import "../assets/css/notes.scss";
-import Loader from "../components/common/Loader";
 import CreateNote from "../components/notes/CreateNote";
 import NoteList from "../components/notes/NoteList";
+import NotesHeader from "../components/notes/NotesHeader";
 import { useAppDispatch, useAppSelector } from "../hooks/redux";
 import { updateCurrentNote } from "../reducers/notes/notesSlice";
 import { fetchNote, updateNote } from "../services/notes/notesServices";
@@ -15,7 +13,7 @@ import { RootState } from "../store/store";
 
 const Notes = () => {
   const dispatch = useAppDispatch();
-  const { notes, trash, activeNoteId, status, action } = useAppSelector(
+  const { notes, trash, activeNoteId } = useAppSelector(
     (state: RootState) => state.notes,
   );
   const note = useMemo(() => {
@@ -74,74 +72,14 @@ const Notes = () => {
     <div className="notes-container">
       <NoteList show={showNoteList} />
       <div className="notes flex-1">
-        <div className="notes-header">
-          <div className="flex items-center">
-            {!showNoteList && (
-              <Button
-                color="dark"
-                size={"xs"}
-                className="md:hidden btn-save-note"
-                onClick={() => setShowNoteList(!showNoteList)}
-              >
-                <FiMenu size={20} />
-              </Button>
-            )}
-            {showNoteList && (
-              <Button
-                color="dark"
-                size={"xs"}
-                className="md:hidden btn-save-note btn-notes-list-close"
-                onClick={() => setShowNoteList(false)}
-              >
-                <FiX size={20} />
-              </Button>
-            )}
-            {note?.name ? (
-              <span className="notes-header-item">
-                {note?.name ?? ""}
-                {note?.isSaved === false && <sup className="text-sm">*</sup>}
-              </span>
-            ) : null}
-            <Tooltip content="Create new note" className="z-[9999]">
-              <Button
-                color="warning"
-                size={"xs"}
-                className="btn-create-note"
-                onClick={() => setShowCreateNote(true)}
-              >
-                <FiPlus size={20} />
-              </Button>
-            </Tooltip>
-          </div>
-          <div className="flex gap-1">
-            <Tooltip content="Reload" className="z-[9999]">
-              <Button
-                size={"xs"}
-                color="warning"
-                className="btn-save-note"
-                onClick={reloadNote}
-                isProcessing={status === "pending" && action === "fetch"}
-                processingSpinner={<Loader size={"sm"} />}
-                disabled={(status === "pending" && action === "fetch") || !note || note.isDeleted}
-              >
-                <FiRotateCw size={20} />
-              </Button>
-            </Tooltip>
-            <Tooltip content="Save" className="z-[9999]">
-              <Button
-                size={"xs"}
-                color="success"
-                className="btn-save-note"
-                onClick={saveNote}
-                isProcessing={status === "pending" && action === "save"}
-                processingSpinner={<Loader size={"sm"} />}
-                disabled={(status === "pending" && action === "save") || !note || note.isDeleted}
-              >
-                <FiSave size={20} />
-              </Button>
-            </Tooltip>
-          </div>
-        </div>
+        <NotesHeader
+          note={note}
+          reloadNote={reloadNote}
+          saveNote={saveNote}
+          createNote={() => setShowCreateNote(true)}
+          showNoteList={showNoteList}
+          toggleNoteList={setShowNoteList}
+        />
         <CKEditor
           key={note?.id ?? "ckeditor"}
           editor={ClassicEditor}
